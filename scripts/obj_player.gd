ac36ccdf7dbcc58e515b4f61cda4d17995e1e791
Herodegon <ronaldjock45@gmail.com>
2025-05-16 06:35:26 -0400

    Fixed bug where going into drive from reverse would set velocity to 0, fixed bug that caused back wheels to move closer towards front wheels as velocity increased

--- scripts/obj_player.gd
+++ scripts/obj_player.gd
@@ -33,22 +33,23 @@ const debug_colors := [
 ]
 
 var curr_state := CarStates.DRIVE
+var state_max := 30.0
+var state_min := 0.0
 
 @export_group("Camera")
 @export var camera_offset := Vector3(0.0,10.0,0.0)
 
 @export_group("Movement")
-@export var max_speed := 50.0
-@export var reverse_speed := 5.0
-@export var max_acceleration := 2.5
+@export var max_speed := 30.0
+@export var min_speed := -10.0
+@export var max_acceleration := 16.0
 @export var turn_angle := PI/6
-@export var friction := 0.7
-
-@export_group("Wheels")
-@export var wheel_radius := 0.5
-@export var wheel_width := 0.2
+@export var friction := -0.2
+@export var drag := -0.01
 
 @onready var camera := $Camera3D
+
+var iter := 0
 
 ## Creates 3D lines in real time for visualizing velocity, acceleration, position, etc.
 ## Meshes instantiated are added to the root tree, and must be managed/freed manually
@@ -62,109 +63,146 @@ func clear_debug_nodes() -> void:
 		node.queue_free()
 	debug_nodes.clear()
 
+## Recursively calculates the top speed of the car using given friction and drag coefficients
+## Top speed can be defined as V_n = (F_Drag * V_(n-1)^2) + (F_Friction * V_(n-1)) + V_(n-1) + a
+func calc_top_speed(input_velocity: Vector3) -> float:
+	var new_friction := calc_friction(input_velocity)
+	var new_speed := input_velocity + Vector3(max_acceleration,0,0) + new_friction
+	var delta_velocity := new_speed.length() - input_velocity.length()
+	if (delta_velocity < 0.00001):
+		return new_speed.length()
+	else:
+		print("Iter ", iter, " - ", new_speed.length())
+		iter += 1
+		return calc_top_speed(new_speed)
+
 func move_camera(new_position: Vector3) -> void:
 	camera.global_transform.origin = new_position
 
-func change_state(velocity_y: float,dir_y: float) -> void:
-	match(curr_state):
+func change_state(state: CarStates, velocity_y: float,dir_y: float) -> CarStates:
+	match(state):
 		CarStates.DRIVE:
-			#print("DRIVE")
 			if (velocity_y == 0.0 and dir_y == 0.0):
-				curr_state = CarStates.NEUTRAL
+				state = CarStates.NEUTRAL
+				print("New State: NEUTRAL")
+			if (velocity_y < 0.0 and dir_y < 0.0):
+				state = CarStates.REVERSE
+				print("New State: REVERSE")
 		CarStates.NEUTRAL:
-			#print("NEUTRAL")
 			if (dir_y > 0.0):
-				curr_state = CarStates.DRIVE
+				state = CarStates.DRIVE
+				print("New State: DRIVE")
 			elif (dir_y < 0.0):
-				curr_state = CarStates.REVERSE
+				state = CarStates.REVERSE
+				print("New State: REVERSE")
 		CarStates.REVERSE:
-			#print("REVERSE")
 			if (dir_y >= 0.0):
-				curr_state = CarStates.NEUTRAL
+				state = CarStates.NEUTRAL
+				print("New State: NEUTRAL")
+
+	return state
+
+func set_speed(state: CarStates) -> void:
+	match(state):
+		CarStates.DRIVE:
+			state_max = max_speed
+			state_min = 0.0
+		CarStates.NEUTRAL:
+			state_max = 0.0
+			state_min = 0.0
+		CarStates.REVERSE:
+			state_max = 0.0
+			state_min = min_speed
 
 func calc_steering(angle: float, player_turn_dir: Vector3, player_accel_dir: Vector3) -> Vector3:
-	# Used to decide car handling at steering angles greater than max_turn_amount
-	#var max_turn_amount = (forward_velocity/max_speed) * 0.8
+	if (curr_state == CarStates.REVERSE):
+		angle *= -1.0
+	# Used to decide car handling at steering angles greater than max_turn_amount (for oversteering and understeering)
+	#var max_turn_amount = (prev_velocity/max_speed) * 0.8
 	var steering_x = player_turn_dir * cos(angle)
 	var steering_y = player_accel_dir * sin(angle)
 	# Turning is perpendicular to and dependent on the direction the car is facing
 	var steering = steering_x + steering_y
-	#print("Difference: ", (180/PI)*angle_difference(atan2(steering.z, steering.x), atan2(player_turn_dir.z, player_turn_dir.x)))
 
 	return steering 
 
+func calc_friction(obj_velocity: Vector3) -> Vector3:
+	var frictional_force = obj_velocity * friction
+	var drag_force = obj_velocity * obj_velocity.length() * drag
+	var net_friction = frictional_force + drag_force
+
+	return net_friction
+
+func _ready() -> void:
+	var top_speed := calc_top_speed(velocity)
+	print("Top Speed: ", top_speed)
+
 func _physics_process(delta: float) -> void:
+	# Reset debug nodes each frame to prevent clutter
 	clear_debug_nodes()
 
-	var pos := global_transform.origin
-
 	# Get raw input from the player
-	var raw_dir := Input.get_vector("turn_left","turn_right","brake","accelerate")
+	var raw_dir := Input.get_vector("turn_left", "turn_right", "brake", "accelerate")
 	var input_dir := Vector3(raw_dir.x, 0.0, -raw_dir.y)
-	#var relative_input := basis * input_dir
 
 	# Calculate forward movement based on the direction the car is facing
 	var forward_dir := -basis.z
 	var accel_dir := input_dir.z * basis.z
 	var turn_dir := input_dir.x * basis.x
 
-	var forward_velocity = velocity.length()
-	change_state(forward_velocity,-input_dir.z)
+	var prev_velocity = velocity.dot(forward_dir)
+	var prev_state = curr_state
+	curr_state = change_state(curr_state, prev_velocity, -input_dir.z)
+	if (curr_state != prev_state):
+		set_speed(curr_state)
 
-	# Calculate the player's max velocity based on the current state
-	var max_player_velocity := Vector3.ZERO
-	match (curr_state):
-		CarStates.DRIVE:
-			if (forward_velocity < 0.0):
-				max_player_velocity = Vector3.ZERO
-			else:
-				max_player_velocity = accel_dir * max_speed
-		CarStates.REVERSE:
-			max_player_velocity = accel_dir * reverse_speed
-		CarStates.NEUTRAL:
-			max_player_velocity = Vector3.ZERO
-
+	#!! BUG: Turning causes a small amount of friction to be applied to the car every frame, even when net friction is 0
 	var wheel_steering := Vector3.ZERO
-	var front_wheel := (scale.z/4.0) * forward_dir
-	var back_wheel := (scale.z/4.0) * -forward_dir
-	if (forward_velocity != 0.0 && !turn_dir.is_zero_approx()):
+	var front_wheel := global_position + (forward_dir * (scale.z/2.0))
+	var back_wheel := global_position - (forward_dir * (scale.z/2.0))
+	if (prev_velocity != 0.0 && !turn_dir.is_zero_approx()):
 		wheel_steering = calc_steering(turn_angle, turn_dir, accel_dir)
 
-	#velocity = accel_dir * 15.0
-
-	front_wheel += wheel_steering * delta
+	# Set velocity for each pair of wheels depending on their role
+	front_wheel += (velocity + wheel_steering) * delta
 	back_wheel += velocity * delta
-
-	print("Wheel Steering: ", wheel_steering.length())
-
 	var new_heading = (front_wheel - back_wheel).normalized()
-	velocity = (new_heading * velocity.length()) + (max_acceleration * accel_dir * delta) - (friction * velocity.normalized() * delta)
-	print("Velocity: ", velocity)
-	if (velocity.length() > max_speed):
-		velocity = velocity.normalized() * max_speed
-	elif (velocity.length() < -reverse_speed):
-		velocity = velocity.normalized() * -reverse_speed
 
-	#move_camera(camera_offset + pos + Vector3(0.0, forward_velocity * 0.1, 0.0))
-	move_camera((1.0 * camera_offset) + pos)
+	# Calculate player velocity
+	#var net_friction = calc_friction(velocity)
+	var net_friction = Vector3.ZERO
+	velocity = (new_heading * prev_velocity) + (max_acceleration * accel_dir * delta) + (net_friction * delta)
+	
+	var curr_velocity = velocity.dot(forward_dir)
+	if (!accel_dir.is_zero_approx()):
+		if (curr_velocity > state_max):
+			print("PeePee")
+			velocity = velocity.normalized() * abs(state_max)
+		# Ensure that the car does not start at velocity of 0 when shifting to drive
+		elif (accel_dir.z > 0.0 && curr_velocity < state_min):
+			print("PooPoo")
+			velocity = velocity.normalized() * abs(state_min)
+
+	move_camera(camera_offset + global_position + Vector3(0.0, (velocity.length()/max_speed) * 5.0, 0.0))
+	#move_camera((1.0 * camera_offset) + global_position)
+
 	move_and_slide()
-
-	if (curr_state == CarStates.DRIVE && forward_velocity > 0.0):
+	if (curr_velocity > 0.0):
 		look_at(global_position + velocity)
-	elif (curr_state == CarStates.REVERSE || forward_velocity < 0.0):
+	elif (curr_velocity < 0.0):
 		look_at(global_position - velocity)
 
 	var debug_obj := [
-		#velocity,
+		velocity,
 		#relative_input,
-		#accel_dir,
-		#turn_dir,
-		#wheel_steering,
-		#max_player_velocity,
-		#front_wheel,
-		#back_wheel,
+		accel_dir,
+		turn_dir,
+		wheel_steering,
 		#-global_transform.basis.x.normalized(),
 		#-global_transform.basis.z.normalized(),
 	]
 
-	draw_debug_lines(global_transform.origin + Vector3(0.0,5.0,0.0),debug_obj,debug_colors)
+	draw_debug_lines(global_position + Vector3(0.0,5.0,0.0),debug_obj,debug_colors)
+
+	draw_debug_lines(global_position + Vector3(0.0,5.0,0.0),[front_wheel - global_position],[Color.GRAY])
+	draw_debug_lines(global_position + Vector3(0.0,5.0,0.0),[back_wheel - global_position],[Color.WHITE])
